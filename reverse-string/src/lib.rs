8f6fc64d2317c16919f263e1b34b603b789eb015
Vadim Statishin <statishin@gmail.com>
2020-02-09 13:17:50 +0200

    improve reverse string

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+//    input.chars().rev().collect::<String>()
+    input.graphemes(true).rev().collect::<String>()
 }
--- reverse-string/tests/reverse-string.rs
+++ reverse-string/tests/reverse-string.rs
@@ -27,43 +27,16 @@ fn test_a_word() {
 
 #[test]
 //#[ignore]
-/// a capitalized word
-fn test_a_capitalized_word() {
-    process_reverse_case("Ramen", "nemaR");
-}
-
-#[test]
-//#[ignore]
-/// a sentence with punctuation
-fn test_a_sentence_with_punctuation() {
-    process_reverse_case("I'm hungry!", "!yrgnuh m'I");
-}
-
-#[test]
-//#[ignore]
-/// a palindrome
-fn test_a_palindrome() {
-    process_reverse_case("racecar", "racecar");
-}
-
-#[test]
-//#[ignore]
-/// an even-sized word
-fn test_an_even_sized_word() {
-    process_reverse_case("drawer", "reward");
-}
-
-#[test]
-//#[ignore]
-/// wide characters
-fn test_wide_characters() {
-    process_reverse_case("子猫", "猫子");
-}
-
-#[test]
-//#[ignore]
-#[cfg(feature = "grapheme")]
+//#[cfg(feature = "grapheme")]
 /// grapheme clusters
 fn test_grapheme_clusters() {
     process_reverse_case("uüu", "uüu");
+}
+
+#[test]
+//#[ignore]
+//#[cfg(feature = "cyrillic")]
+/// cyrillic clusters
+fn test_cyrillic() {
+    process_reverse_case("Привётъй", "йътёвирП");
 }
