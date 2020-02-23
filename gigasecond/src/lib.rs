ebf95e58fb3b0d0fa3dccfce53f450e7b5a87573
Vadim Statishin <statishin@gmail.com>
2020-02-23 17:55:00 +0200

    fix: implement gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,11 +1,8 @@
 use chrono::{DateTime, Utc};
 
-const GIGASECONDS: i64 = 1000000000;
+const GIGASECONDS: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let ts = start.naive_utc().timestamp() + GIGASECONDS;
-    let nt = chrono::NaiveDateTime::from_timestamp(ts, 0);
-    let res = chrono::DateTime::from_utc(nt, *start.offset());
-    res
+    start + chrono::Duration::seconds(GIGASECONDS)
 }
