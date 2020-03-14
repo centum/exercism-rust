f296ed0bb1812f87714ed5c2dc82a778e4c782dd
Vadim Statishin <statishin@gmail.com>
2020-03-14 17:27:33 +0200

    clock: improve code

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,44 +1,29 @@
 use std::fmt;
 
+#[derive(Debug,PartialEq)]
 pub struct Clock {
-    m: i32
+    minutes: i32
 }
 
 const DAY_MINUTES: i32 = 24 * 60;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut m = (hours*60 + minutes) % DAY_MINUTES;
-        if m < 0 {
-            m += DAY_MINUTES
-        }
-        Clock{m}
+        Clock{ minutes: (hours*60 + minutes).rem_euclid(DAY_MINUTES) }
     }
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(0, self.m + minutes)
+        Clock::new(0, self.minutes + minutes)
     }
-    pub fn hour(&self) -> i32 {
-        self.m / 60
+    fn hour(&self) -> i32 {
+        self.minutes / 60
     }
-    pub fn minute(&self) -> i32 {
-        self.m % 60
+    fn minute(&self) -> i32 {
+        self.minutes % 60
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:0>2}:{:0>2}", self.hour(), self.minute())
-    }
-}
-
-impl fmt::Debug for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Clock {:0>2}:{:0>2}", self.hour(), self.minute())
-    }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.m == other.m
     }
 }
